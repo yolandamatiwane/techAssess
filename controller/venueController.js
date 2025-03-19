9d90e4b5fa0d0eae366de1e1f43e78b34471b1ab
yolandamatiwane <mbalentle.matiwane21@gmail.com>
2025-03-19 23:41:16 +0200

    made venue data return workshop details

--- controller/venueController.js
+++ controller/venueController.js
@@ -1,7 +1,7 @@
-import { getVenueDb } from "../model/venue.js";
+import { getVenueWorkshopsDb } from "../model/venue.js";
 
 const fetchVenues = async(req,res)=>{
-    let venueDb = await getVenueDb();
+    let venueDb = await getVenueWorkshopsDb();
     res.status(200).json(venueDb)
 }
 
--- model/venue.js
+++ model/venue.js
@@ -1,12 +1,16 @@
 import {pool} from '../config/config.js'
 
 
-const getVenueDb = async ()=>{
+const getVenueWorkshopsDb = async ()=>{
     let [data] = await pool.query('SELECT * FROM venues');
+    for(let venue of data){
+        let [workshops] = await pool.query('SELECT * FROM workshops WHERE venue_id = ?', [venue.venue_id]);
+        venue.workshops = workshops;
+    }
     return data;
 }
 
 export {
-    getVenueDb
+    getVenueWorkshopsDb
 }
 
